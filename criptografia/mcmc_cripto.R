d536a95996f75d5442e785281f6d28ce0f857326
Pedro Garcia <84725489+PedroGarcia6@users.noreply.github.com>
2023-12-04 17:15:05 -0300

    Add files via upload

--- criptografia/mcmc_cripto.R
+++ criptografia/mcmc_cripto.R
@@ -1,13 +1,13 @@
 library('reshape2')
 library('ggplot2')
 
-# LÃª o texto e converte todas as letras para maiÃºsculas
-reference=readLines("C:/Users/Pedro/Desktop/FGV/GRADUAÃ‡ÃƒO/6Âº PERÃODO/PROCESSOS ESTOCÃSTICOS/TRABALHO EM GRUPO/CRIPTOGRAFIA/la_divina_comedia.txt")
+# Lê o texto e converte todas as letras para maiúsculas
+reference=readLines("C:/Users/Pedro/Desktop/FGV/GRADUAÇÃO/6º PERÍODO/PROCESSOS ESTOCÁSTICOS/TRABALHO EM GRUPO/CRIPTOGRAFIA/sob_a_redoma.txt")
 reference=toupper(reference)
 
-# Inicializa uma matriz de transiÃ§Ã£o entre letras do alfabeto.
-# Percorre o texto de referÃªncia e conta a frequÃªncia de transiÃ§Ã£o de uma letra para outra. 
-# Essa informaÃ§Ã£o serÃ¡ usada para calcular as probabilidades de transiÃ§Ã£o.
+# Inicializa uma matriz de transição entre letras do alfabeto.
+# Percorre o texto de referência e conta a frequência de transição de uma letra para outra. 
+# Essa informação será usada para calcular as probabilidades de transição.
 trans.mat=matrix(0,27,27)
 rownames(trans.mat)=colnames(trans.mat)=c(toupper(letters),"")
 lastletter=""
@@ -37,17 +37,17 @@ for (ln in 1:length(reference)) {
   lastletter=""
 }
 
-# Calcula as probabilidades de transiÃ§Ã£o a partir da matriz de contagem.
+# Calcula as probabilidades de transição a partir da matriz de contagem.
 trans.prob.mat=sweep(trans.mat+1,1,rowSums(trans.mat+1),FUN="/")
 
-# Cria um heatmap que representa visualmente as probabilidades de transiÃ§Ã£o entre letras do alfabeto.
+# Cria um heatmap que representa visualmente as probabilidades de transição entre letras do alfabeto.
 ggplot(melt(trans.prob.mat),aes(Var2,Var1))+geom_tile(aes(fill=value))+
   scale_fill_gradient(low="skyblue",high="darkblue",limits=c(0,1))+
   labs(x="Letra seguinte",y="Letra atual",fill="Prob")+
   scale_y_discrete(limits = rev(levels(melt(trans.prob.mat)$Var1)))+
   coord_equal()
 
-# Define uma funÃ§Ã£o para decodificar um texto usando um mapeamento especÃ­fico de letras.
+# Define uma função para decodificar um texto usando um mapeamento específico de letras.
 decode <- function(mapping,coded) {
   coded=toupper(coded)
   decoded=coded
@@ -59,7 +59,7 @@ decode <- function(mapping,coded) {
   decoded
 }
 
-# Calcula a log probabilidade de um texto decodificado com base nas probabilidades de transiÃ§Ã£o.
+# Calcula a log probabilidade de um texto decodificado com base nas probabilidades de transição.
 log.prob <- function(mapping,decoded) {
   logprob=0
   
@@ -86,21 +86,19 @@ log.prob <- function(mapping,decoded) {
 }
 
 # Define um texto original e o codifica aleatoriamente para criar um texto criptografado.
-
-#correctTxt="ENTER HAMLET HAM TO BE OR NOT TO BE THAT IS THE QUESTION WHETHER TIS NOBLER IN THE MIND TO SUFFER THE SLINGS AND ARROWS OF OUTRAGEOUS FORTUNE OR TO TAKE ARMS AGAINST A SEA OF TROUBLES AND BY OPPOSING END"
-#correctTxt="UM ENORME SORRISO LHE ORNAVA O FOCINHO AS ORELHAS ESTAVAM PARA TRAS GRUDADAS NO CRANIO A SOMBRA CORRIA AO SEU LADO NO CAPIM MANCHADO DE FULIGEM JULIA SE AJOELHOU E ESTENDEU OS BRACOS"
-correctTxt="Quella circulazion che sÃ¬ concetta pareva in te come lume reflesso, da li occhi miei alquanto circunspetta, dentro da sÃ©, del suo colore stesso, mi parve pinta de la nostra effige"
-#correctTxt="MAS TALVEZ FOSSE UM ABISMO EM QUE HOMENS MELHORES DO QUE ELE TIVESSEM CAIDO SE ELA TIVESSE RECONSIDERADO TUDO NA SUA VIDA DAI PARA A FRENTE TERIA MUDADO PORQUE ELA DEVE TER CONSEGUIDO SAIR NUNCA MAIS ELE VIU A LOURA DE ROSTO VICOSO NEM O FORD VELHO E SUJO"
-#correctTxt = "Ainda cheguei a tempo para incluir no inventÃ¡rio da lÃ­ngua nacional milhares de verbas, que Ã manhan estariam sonegadas por essa insaciÃ¡vel cabeÃ§a de casal, que se chama civilizaÃ§Ã£o, o que, desculpando-se com os seus intuitos nobilÃ­ssimos, procura locupletar-se Ã¡ custa de tudo e apesar de tudo."
-#correctTxt = "Mi iris al la fundamento hodiaÅ­ kaj alvenis tute malseka Ä‰ar multe pluvis"
+correctTxt="A dois mil pés, onde Claudette Sanders recebia uma aula de voo, a cidade de Chester's Mill
+cintilava à luz da manhã como algo que acabou de ficar pronto e de ser ali pousado. Os carros rodavam
+pela rua principal, relampejando piscadelas de sol. A torre da Igreja Congregacional parecia tão aguda
+que poderia furar o céu imaculado. O sol correu pela superfície do riacho Prestile quando o Seneca V o
+sobrevoou, avião e água cortando a cidade na mesma rota diagonal"
 coded=decode(sample(toupper(letters)),correctTxt)
 
-# Inicializa um mapeamento aleatÃ³rio e itera sobre tentativas de permutaÃ§Ã£o.
-# Usa o algoritmo Metropolis-Hastings para aceitar ou rejeitar permutaÃ§Ãµes com base em probabilidades.
-# O objetivo Ã© encontrar o mapeamento que maximiza a log probabilidade do texto decodificado.
+# Inicializa um mapeamento aleatório e itera sobre tentativas de permutação.
+# Usa o algoritmo Metropolis-Hastings para aceitar ou rejeitar permutações com base em probabilidades.
+# O objetivo é encontrar o mapeamento que maximiza a log probabilidade do texto decodificado.
 mapping=sample(toupper(letters))
 i=1
-iters=1500
+iters=500
 cur.decode=decode(mapping,coded)
 cur.loglike=log.prob(mapping,cur.decode)
 max.loglike=cur.loglike
